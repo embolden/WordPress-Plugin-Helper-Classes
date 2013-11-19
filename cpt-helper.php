ea893f13d377d93f79b232dd65e9eb54f0bfb95c
Matthew Keehner <matthewkeehner@gmail.com>
2013-11-18 21:09:24 -0600

    Changed name variable to singular.  More semantic.

--- cpt-helper.php
+++ cpt-helper.php
@@ -5,34 +5,40 @@
 if( ! class_exists( 'CustomPostType' ) ) :
 class CustomPostType {
 
-	public $post_type_name;
+	public $post_type_singular;
 	public $post_type_plural;
 	public $post_type_args;
 
-	function __construct( $name, $plural, $args = array() ) {
-		$this->post_type_name = $name;
-		$this->post_type_plural = $plural;
-		$this->post_type_args = $args;
+	/*
+	 * @todo : Document me!
+	 */
+	function __construct( $singular, $plural, $args = array() ) {
+		$this->post_type_singular = $singular;
+		$this->post_type_plural   = $plural;
+		$this->post_type_args     = $args;
 
-		if( ! post_type_exists( strtolower( $name ) ) ) {
+		if( ! post_type_exists( strtolower( $singular ) ) ) {
 			add_action( 'init', array( $this, 'register_post_type' ) );
 		}
 	}
 
+	/*
+	 * @todo : Document me!
+	 */
 	function register_post_type() {
-		$name = $this->post_type_name;
-		$plural = $this->post_type_plural;
+		$singular = $this->post_type_singular;
+		$plural   = $this->post_type_plural;
 
 		$labels = array(
 			'name'                => _x( $plural, 'Post Type General Name', '_s' ),
-			'singular_name'       => _x( $name, 'Post Type Singular Name', '_s' ),
+			'singular_name'       => _x( $singular, 'Post Type Singular Name', '_s' ),
 			'menu_name'           => __( $plural, '_s' ),
-			'parent_item_colon'   => __( 'Parent ' . $name . ':', '_s' ),
+			'parent_item_colon'   => __( 'Parent ' . $singular . ':', '_s' ),
 			'all_items'           => __( 'All ' . $plural, '_s' ),
-			'view_item'           => __( 'View ' . $name, '_s' ),
-			'add_new_item'        => __( 'Add New ' . $name, '_s' ),
-			'add_new'             => __( 'New ' . $name, '_s' ),
-			'edit_item'           => __( 'Edit ' . $name , '_s' ),
+			'view_item'           => __( 'View ' . $singular, '_s' ),
+			'add_new_item'        => __( 'Add New ' . $singular, '_s' ),
+			'add_new'             => __( 'New ' . $singular, '_s' ),
+			'edit_item'           => __( 'Edit ' . $singular , '_s' ),
 			'update_item'         => __( 'Update ' . $plural, '_s' ),
 			'search_items'        => __( 'Search ' . $plural, '_s' ),
 			'not_found'           => __( 'No ' . strtolower( $plural ) . ' found', '_s' ),
@@ -67,7 +73,7 @@ class CustomPostType {
 
 		$args = wp_parse_args( $this->post_type_args, $defaults );
 
-		register_post_type( str_replace( ' ', '-', strtolower( $name ) ), $args );
+		register_post_type( str_replace( ' ', '-', strtolower( $singular ) ), $args );
 	}
 }
 endif;
